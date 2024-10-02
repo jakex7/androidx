b9c25d6c988a42b2da4a2048c61412a979f9bd09
Aurimas Liutikas <aurimas@google.com>
2024-10-02 12:53:14 -0700

    Upgrade to protobuf 4.28.2
    - Remove unused protobufKotlin entry
    - Keep baseline-profile-gradle-plugin on the existig protobuf library
      as it causing AGP crashes
    
    Bug: 371158364
    Bug: 368091265
    Test: ./gradlew bOS --dry-run
    (cherry picked from https://android-review.googlesource.com/q/commit:e53df81da8bdc61e77d380cb511f8bdbeea27849)
    (cherry picked from https://android-review.googlesource.com/q/commit:3d32590ac09c1c1e84d17b25911b249916d4fb0f)
    Merged-In: I6765e62bfa4462a5b4b56ae9171394a04f0e0219
    Change-Id: I6765e62bfa4462a5b4b56ae9171394a04f0e0219

--- buildSrc/private/src/main/kotlin/androidx/build/AndroidXRepackageImplPlugin.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/AndroidXRepackageImplPlugin.kt
@@ -50,20 +50,19 @@ class AndroidXRepackageImplPlugin : Plugin<Project> {
         createConfigurations()
 
         val sourceSets = extensions.getByType(SourceSetContainer::class.java)
-        val libraryShadowJar = tasks.register(
-            "shadowLibraryJar", ShadowJar::class.java
-        ) { task ->
-            task.transformers.add(
-                BundleInsideHelper.DontIncludeResourceTransformer().apply {
-                    dropResourcesWithSuffix = ".proto"
-                }
-            )
-            task.from(sourceSets.findByName("main")?.output)
-        }
-
-        afterEvaluate {
-            val artifactIdForPublish = relocationExtension.artifactId
-            libraryShadowJar.configure { task ->
+        val libraryShadowJar =
+            tasks.register("shadowLibraryJar", ShadowJar::class.java) { task ->
+                task.transformers.add(
+                    BundleInsideHelper.DontIncludeResourceTransformer().apply {
+                        dropResourcesWithSuffix = ".proto"
+                    }
+                )
+                task.transformers.add(
+                    BundleInsideHelper.DontIncludeResourceTransformer().apply {
+                        dropResourcesWithSuffix = ".proto.bin"
+                    }
+                )
+                task.from(sourceSets.named("main").map { it.output })
                 relocationExtension.getRelocations().forEach {
                     task.relocate(it.sourcePackage, it.targetPackage)
                 }
